4d2142c01d33f77d26192dba066b3ab40dbe80c1
omaremadcc <om3x4e@gmail.com>
2026-03-06 20:51:42 +0200

    Added Our story section

--- app/Components/HeroPage/Hero.tsx
+++ app/Components/HeroPage/Hero.tsx
@@ -17,7 +17,7 @@ export default function Hero() {
         <h1 className="text-4xl md:text-6xl font-extrabold mt-2 mb-5">
           Egypt’s Smart City
         </h1>
-        <p className="text-base md:text-lg text-muted-text px-3 lg:max-w-[60%] lg:max-w-[40%]">
+        <p className="text-base md:text-lg text-muted-text px-3 lg:max-w-[40%]">
           <span className="text-semi-muted-text">ACUD</span>, founded in{" "}
           <span className="text-semi-muted-text">2016</span>, leads the
           development of{" "}
---
+++ app/about/page.tsx
@@ -0,0 +1,42 @@
+export default function Page() {
+  return (
+    <div>
+      <div className="w-screen h-screen flex justify-start items-end px-3 py-2">
+        <div>
+          <h4 className="text-muted-text text-4xl">
+            Build Egypt's New Capital
+          </h4>
+          <h1 className="text-[112px] font-extrabold leading-none">
+            ABOUT ACUD
+          </h1>
+        </div>
+      </div>
+      <div className="flex gap-10 justify-center items-start flex-col-reverse md:flex-row py-20 responsive-container">
+        <div className="">
+          <span className="text-lg md:text-2xl text-semi-muted-text font-semibold">
+            Our Story
+          </span>
+          <h2 className="font-semibold text-3xl md:text-5xl">
+            Leading Egypt's Urban Transformation
+          </h2>
+          <p className="text-muted-text md:text-lg">
+            ACUD, founded in 2016, is the driving force behind Egypt's most
+            ambitious urban development project - the New Capital. Our mission
+            is to create a world-class, sustainable city that serves as the new
+            political and administrative hub of Egypt.
+          </p>
+          <p className="text-muted-text md:text-lg mt-5">
+            We are committed to excellence, innovation, and sustainable
+            development, working with global partners to build a city that meets
+            the highest international standards.
+          </p>
+        </div>
+        <img
+          src="/about/1.webp"
+          alt="New Capital"
+          className=" max-w-[40%] h-auto"
+        />
+      </div>
+    </div>
+  );
+}
