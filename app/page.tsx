c461cc1044d9cbbb44c41a04292b229a8ef33320
omaremadcc <om3x4e@gmail.com>
2026-03-06 17:45:33 +0200

    Feat: Added Testimonials Section in the main menu

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,19 @@ import CapitalDistricts from "./Components/HeroPage/CapitalDistricts";
 import Hero from "./Components/HeroPage/Hero";
 import NewCapital from "./Components/HeroPage/NewCapital";
 import Parteners from "./Components/HeroPage/Parteners";
+import Testimonials from "./Components/HeroPage/Testimonials";
 import Navbar from "./Components/Navbar";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="mb-60">
       <Navbar />
       <Hero />
       <Awards />
       <NewCapital />
       <CapitalDistricts />
       <Parteners />
+      <Testimonials />
     </div>
   );
 }
