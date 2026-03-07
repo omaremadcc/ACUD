218353714c3e9dd6c259ead86f2d988bbc1a76de
omaremadcc <om3x4e@gmail.com>
2026-03-07 21:05:09 +0200

    Add RevealText component to new capital page

--- app/new-capital/page.tsx
+++ app/new-capital/page.tsx
@@ -1,18 +1,21 @@
 import NewCapitalAtGlance from "../Components/About/NewCapitalAtGlance";
 import CapitalPillars from "../Components/NewCapital/CapitalPillars";
 import NewCapitalCap from "../Components/NewCapital/NewCapitalCap";
+import RevealText from "../Components/RevealText";
 
 export default function NewCapital() {
   return (
     <div>
-      <div className="w-screen h-screen flex justify-start items-end px-3 py-2">
+      <div className="w-screen h-screen flex justify-center md:justify-start items-end px-3 py-2">
         <div>
-          <h4 className="text-muted-text text-4xl">
-            Egypt's Future City - A World-Class Smart Hub
-          </h4>
-          <h1 className="text-[112px] font-extrabold leading-none">
-            New Capital
-          </h1>
+          <RevealText delay={0.4} duration={0.6}>
+            <h4 className="hero-subheading">
+              Egypt's Future City - A World-Class Smart Hub
+            </h4>
+          </RevealText>
+          <RevealText delay={0.2} duration={0.6}>
+            <h1 className="hero-heading">New Capital</h1>
+          </RevealText>
         </div>
       </div>
       <NewCapitalAtGlance />
