a9234512336796f78a9782673eae02d1ae82e5ab
omaremadcc <om3x4e@gmail.com>
2026-03-10 15:09:04 +0200

    Add Vercel analytics integration
    Integrate Vercel's analytics package to enable tracking of website
    traffic and user behavior.

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +35,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
